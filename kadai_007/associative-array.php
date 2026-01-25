bbe5f42270a4354c9618f527c9611cf1e8f1131f
aayn16 <aayn16.h@gmail.com>
2026-01-25 10:05:06 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎7章課題</title>
+</head>
+
+<body>
+  <p>
+<?php
+$personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+print_r($personal_data);
+//var_dump($personal_data);
+//echo $personal_data['name'];
+
+?>
+
+  </p>
+</body>
