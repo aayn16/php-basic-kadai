bcb3538381e3ffff6cd8eb0d5d3404593947a087
aayn16 <aayn16.h@gmail.com>
2026-01-29 08:33:40 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -9,7 +9,7 @@ $score7 = 80;
 $score8 = 95;
 $score9 = 30;
 $score10 = 60;
-$total = ($score1+$score2+$score3+$score3+$score4+$score5+$score6+$score7+$score8+$score9+$score10);
+$total = ($score1+$score2+$score3+$score4+$score5+$score6+$score7+$score8+$score9+$score10);
 
 echo  $total/10;
 
